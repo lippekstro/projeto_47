d7fd97e5f984fd3fc5b551b04c79dde0cb309c17
anjinhadias <angelavdiasf@gmail.com>
2023-08-03 20:31:25 -0300

    perfil navbar white

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -39,7 +39,7 @@
                         </li>
                        <?php else: ?>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Perfil
                             </a>
                             <ul class="dropdown-menu">

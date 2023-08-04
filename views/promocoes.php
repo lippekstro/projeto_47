2af4d73a5b6182b9d30e8bd03ed1d053aec6d7a1
bugado23 <paulonascomento2@gmail.com>
2023-08-04 16:00:46 -0300

    promoções

---
+++ views/promocoes.php
@@ -0,0 +1,30 @@
+<?php 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/cabecalho.php';
+?>
+
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/cabecalho.php';
+?>
+
+<link rel="stylesheet" href="../css/promoções.css">
+
+<h1>Restaurantes</h1>
+<div class="container">
+    <div class="card_box">
+        <img class="card-img-top"
+            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189c1d9ae84%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189c1d9ae84%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.5%22%20y%3D%2296.6%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            alt="Imagem de capa do card">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis, quia ullam sint modi suscipit.</p>
+        <img src="https://cdn-icons-png.flaticon.com/128/4192/4192747.png" alt="">
+        <span></span>
+    </div>
+</div>
+
+
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/rodape.php';
+?>
+
+<?php 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/rodape.php';
+?>

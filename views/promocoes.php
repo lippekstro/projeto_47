e4a02a097845b61a69ac1177bbc7ec2c6986cdd9
vinisantx <vhiniciussantos7@gmail.com>
2023-08-05 14:58:39 -0300

    cards

---
+++ views/promocoes.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>promoções</title>
+    <link rel="stylesheet" href="/projeto_47/css/promocoes.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- Link Swiper's CSS -->
+<!--      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+ -->     <link rel="stylesheet" href="/projeto_47/js/promocoes.js">
+</head>
+
+<body>
+
+    <section>
+        <div class="conteiner">
+            <div class="conent">
+                  <div class="container-card">
+                    <div class="card_box">
+                        <span></span>
+                        <div class="question-icon">
+                            <i class="bi bi-exclamation-circle-fill"></i>
+                        </div>
+                        <div class="card-img">
+                        </div>
+                        <div class="card-info">
+                            <p class="text-title"> SAL E BRASA</p>
+                            <p class="text-body">Promoção para Anivessariantes</p>
+                        </div>
+                        <div class="info-box">
+                            <p>• Destaque do mês: Prato exclusivo a preço especial</p>
+                            <p>• Família reunida: Refeições infantis ao preço de criança, toda quarta-feira.</p>
+                            <p>• Noite romântica: Jantar para dois com entrada, prato principal e sobremesa por preço especial.</p>
+                        </div>
+                        <a href="#"><button class="button">SAIBA MAIS</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </section>
+
+
+
+
+</body>
+
+</html>

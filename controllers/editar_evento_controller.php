ffbf9b9249d96302b2da37492ea1afb149239888
Daniele Araujo Dos Santos <133619451+heyaraujo@users.noreply.github.com>
2023-08-08 14:45:22 -0300

    Update editar_evento_controller.php

--- controllers/editar_evento_controller.php
+++ controllers/editar_evento_controller.php
@@ -1,38 +1,37 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "projeto_47";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-   
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "projeto_47";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
+    }
+
     function limparDados($dados) {
         return htmlspecialchars(trim($dados));
     }
 
-   
     $id_evento = limparDados($_POST["id_evento"]);
-    $local_evento = limparDados($_POST["local_evento"]);
+    $titulo = limparDados($_POST["tituloEvento"]);
+    $latitude = floatval($_POST["latitudeEvento"]);
+    $longitude = floatval($_POST["longitudeEvento"]);
+    $local_evento = limparDados($_POST["localEvento"]);
     $data_evento = limparDados($_POST["data_evento"]);
     $descricao_evento = limparDados($_POST["descricao_evento"]);
     $preco = isset($_POST["preco"]) ? floatval($_POST["preco"]) : null;
     $link_evento = isset($_POST["link_evento"]) ? limparDados($_POST["link_evento"]) : null;
     $id_categoria = limparDados($_POST["id_categoria"]);
 
-    
-    if (empty($local_evento) || empty($data_evento) || empty($descricao_evento) || empty($id_categoria)) {
+    if (empty($local_evento) || empty($titulo) || empty($data_evento) || empty($id_categoria)) {
         echo "Erro: Todos os campos obrigatórios devem ser preenchidos.";
         exit;
     }
 
-   
-    $sql = "UPDATE eventos SET local_evento = ?, data_evento = ?, descricao_evento = ?, preco = ?, link_evento = ?, id_categoria = ? WHERE id_evento = ?";
+    $sql = "UPDATE eventos SET local_evento = ?, titulo = ?, data_evento = ?, descricao_evento = ?, preco = ?, link_evento = ?, id_categoria = ?, latitude = ?, longitude = ? WHERE id_evento = ?";
     $stmt = $conn->prepare($sql);
 
     if (!$stmt) {
@@ -40,10 +39,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         exit;
     }
 
-   
-    $stmt->bind_param("sssdssi", $local_evento, $data_evento, $descricao_evento, $preco, $link_evento, $id_categoria, $id_evento);
+    $stmt->bind_param("ssssdssddi", $local_evento, $titulo, $data_evento, $descricao_evento, $preco, $link_evento, $id_categoria, $latitude, $longitude, $id_evento);
 
-   
     if ($stmt->execute()) {
         echo "Evento atualizado com sucesso!";
     } else {
@@ -51,6 +48,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     $stmt->close();
+    $conn->close();
 }
-
-$conn->close();
+?>

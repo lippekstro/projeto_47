1c7a34a4035a209e868214e6b29826cc6ca6499c
Anderson-Brendon <137108081+Anderson-Brendon@users.noreply.github.com>
2023-08-09 13:21:41 -0300

    Update sobre.php

--- views/sobre.php
+++ views/sobre.php
@@ -2,30 +2,28 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/cabecalho.php';
 ?>
 
-<section id="sobreSite" class="d-flex flex-column justify-content-center align-items-center mt-5">
-    <div class="card mb-4 mt-3 w-50 h-50">
-        <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-        </div>
+<section id="sobreSite" class="mt-4">
+    <h1 class="text-center">Sobre nós</h1>
+    <div class="m-4 d-flex flex-column justify-content-center align-items-center">
+        <figure>
+            <figcaption class="fs-2 mb-2 text-center">lorem ipsum</figcaption>
+            <img src="https://placehold.co/600x400" class="img-thumbnail img-fluid" alt="...">
+        </figure>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis reiciendis quibusdam eos aut enim debitis voluptatum explicabo nam, dolores sit natus officia? Inventore perspiciatis veritatis debitis impedit tempora consequatur perferendis?</p>
     </div>
-    <div class="card mb-4 mt-3 w-50 h-50">
-        <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-        </div>
+    <div class="m-4 d-flex flex-column justify-content-center align-items-center">
+        <figure>
+            <figcaption class="fs-2 mb-2 text-center">lorem ipsum</figcaption>
+            <img src="https://placehold.co/600x400" class="img-thumbnail img-fluid" alt="...">
+        </figure>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis reiciendis quibusdam eos aut enim debitis voluptatum explicabo nam, dolores sit natus officia? Inventore perspiciatis veritatis debitis impedit tempora consequatur perferendis?</p>
     </div>
-    <div class="card mb-4 mt-3 w-50 h-50">
-        <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-        </div>
+    <div class="m-4 d-flex flex-column justify-content-center align-items-center">
+        <figure>
+            <figcaption class="fs-2 mb-2 text-center">lorem ipsum</figcaption>
+            <img src="https://placehold.co/600x400" class="img-thumbnail img-fluid" alt="...">
+        </figure>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis reiciendis quibusdam eos aut enim debitis voluptatum explicabo nam, dolores sit natus officia? Inventore perspiciatis veritatis debitis impedit tempora consequatur perferendis?</p>
     </div>
 </section>
 

788ae6cf9e352482539e01ab13a03fea49f84431
bugado23 <paulonascomento2@gmail.com>
2023-08-09 16:22:52 -0300

    cards

--- views/promocoes.php
+++ views/promocoes.php
@@ -15,52 +15,37 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/cabecalho.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/promoções.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
     <!-- Certifique-se de ter o caminho correto para o seu arquivo CSS -->
 </head>
 
 <body>
     <div class="containerBody">
         <h1>Restaurantes</h1>
-        
+
         <div class="containerCard">
 
             <div class="container">
-                <div class="card_box">
+                <div class="card" style="width: 18rem;">
                     <span></span>
-                    <img class="card-img-top"
-                        src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189c1d9ae84%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189c1d9ae84%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.5%22%20y%3D%2296.6%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                        alt="Imagem de capa do card">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis, quia ullam sint modi
-                        suscipit.</p>
-                    <img src="\projeto_47\img/question-lg.svg" alt="" class="question-icon">
-                    <div class="info-box">
-                        <p>• Destaque do mês: Prato exclusivo a preço especial</p>
-                        <p>• Família reunida: Refeições infantis ao preço de criança, toda quarta-feira.</p>
-                        <p>• Noite romântica: Jantar para dois com entrada, prato principal e sobremesa por preço
-                            especial.
-                        </p>
+                    <img src="/projeto_47/img/restaurante-sal.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Saiba mais</a>
                     </div>
-                    <button><a href="#">Saiba mais</a></button>
                 </div>
             </div>
-            
+
             <div class="container">
-                <div class="card_box">
+                <div class="card" style="width: 18rem;">
                     <span></span>
-                    <img class="card-img-top"
-                        src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189c1d9ae84%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189c1d9ae84%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.5%22%20y%3D%2296.6%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                        alt="Imagem de capa do card">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis, quia ullam sint modi
-                        suscipit.</p>
-                    <img src="\projeto_47\img/question-lg.svg" alt="" class="question-icon">
-                    <div class="info-box">
-                        <p>• Destaque do mês: Prato exclusivo a preço especial</p>
-                        <p>• Família reunida: Refeições infantis ao preço de criança, toda quarta-feira.</p>
-                        <p>• Noite romântica: Jantar para dois com entrada, prato principal e sobremesa por preço
-                            especial.
-                        </p>
+                    <img src="/projeto_47/img/restaurante-sal.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Saiba mais</a>
                     </div>
-                    <button><a href="#">Saiba mais</a></button>
                 </div>
             </div>
 
@@ -72,43 +57,27 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/cabecalho.php';
 
             <div class="container">
 
-                <div class="card_box">
+                <div class="card" style="width: 18rem;">
                     <span></span>
-                    <img class="card-img-top"
-                        src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189c1d9ae84%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189c1d9ae84%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.5%22%20y%3D%2296.6%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                        alt="Imagem de capa do card">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis, quia ullam sint modi
-                        suscipit.</p>
-                    <img src="\projeto_47\img/question-lg.svg" alt="" class="question-icon">
-                    <div class="info-box">
-                        <p>• Destaque do mês: Prato exclusivo a preço especial</p>
-                        <p>• Família reunida: Refeições infantis ao preço de criança, toda quarta-feira.</p>
-                        <p>• Noite romântica: Jantar para dois com entrada, prato principal e sobremesa por preço
-                            especial.
-                        </p>
+                    <img src="/projeto_47/img/restaurante-sal.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Saiba mais</a>
                     </div>
-                    <button><a href="#">Saiba mais</a></button>
                 </div>
             </div>
 
             <div class="container">
 
-                <div class="card_box">
+                <div class="card" style="width: 18rem;">
                     <span></span>
-                    <img class="card-img-top"
-                        src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189c1d9ae84%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189c1d9ae84%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.5%22%20y%3D%2296.6%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                        alt="Imagem de capa do card">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis, quia ullam sint modi
-                        suscipit.</p>
-                    <img src="\projeto_47\img/question-lg.svg" alt="" class="question-icon">
-                    <div class="info-box">
-                        <p>• Destaque do mês: Prato exclusivo a preço especial</p>
-                        <p>• Família reunida: Refeições infantis ao preço de criança, toda quarta-feira.</p>
-                        <p>• Noite romântica: Jantar para dois com entrada, prato principal e sobremesa por preço
-                            especial.
-                        </p>
+                    <img src="/projeto_47/img/restaurante-sal.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Saiba mais</a>
                     </div>
-                    <button><a href="#">Saiba mais</a></button>
                 </div>
 
 
@@ -121,44 +90,29 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_47/templates/cabecalho.php';
         <div class="containerCard">
 
             <div class="container">
-
-                <div class="card_box">
+                <div class="card" style="width: 18rem;">
                     <span></span>
-                    <img class="card-img-top"
-                        src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189c1d9ae84%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189c1d9ae84%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.5%22%20y%3D%2296.6%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                        alt="Imagem de capa do card">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis, quia ullam sint modi
-                        suscipit.</p>
-                    <img src="\projeto_47\img/question-lg.svg" alt="" class="question-icon">
-                    <div class="info-box">
-                        <p>• Destaque do mês: Prato exclusivo a preço especial</p>
-                        <p>• Família reunida: Refeições infantis ao preço de criança, toda quarta-feira.</p>
-                        <p>• Noite romântica: Jantar para dois com entrada, prato principal e sobremesa por preço
-                            especial.
-                        </p>
+                    <img src="/projeto_47/img/restaurante-sal.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Saiba mais</a>
                     </div>
-                    <button class="buttonCard"><a href="#">Saiba mais</a></button>
                 </div>
+
+
             </div>
 
             <div class="container">
 
-                <div class="card_box">
+                <div class="card" style="width: 18rem;">
                     <span></span>
-                    <img class="card-img-top"
-                        src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_189c1d9ae84%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_189c1d9ae84%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.5%22%20y%3D%2296.6%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                        alt="Imagem de capa do card">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis, quia ullam sint modi
-                        suscipit.</p>
-                    <img src="\projeto_47\img/question-lg.svg" alt="" class="question-icon">
-                    <div class="info-box">
-                        <p>• Destaque do mês: Prato exclusivo a preço especial</p>
-                        <p>• Família reunida: Refeições infantis ao preço de criança, toda quarta-feira.</p>
-                        <p>• Noite romântica: Jantar para dois com entrada, prato principal e sobremesa por preço
-                            especial.
-                        </p>
+                    <img src="/projeto_47/img/restaurante-sal.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Saiba mais</a>
                     </div>
-                    <button><a href="#">Saiba mais</a></button>
                 </div>
 
 
